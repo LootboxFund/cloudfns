6d1fe24f7d1765a28455e28d7979003338cca079
0xterran <0xterran@protonmail.com>
2022-02-04 13:01:01 -0500

    added semvar

--- pipedream/src/types/semvar.types.ts
+++ pipedream/src/types/semvar.types.ts
@@ -1,4 +1,4 @@
-export type SemanticVersion = "0.0.1-sandbox";
+export type SemanticVersion = "0.0.1-sandbox" | "0.0.1-staging";
 export type GBucketName = "guildfx-exchange.appspot.com";
 
 export interface TerraSemvar {
@@ -8,5 +8,6 @@ export interface TerraSemvar {
 export interface GCloud_Config {
   bucketName: "guildfx-exchange.appspot.com";
   prefixes: GBucketPrefixes;
+  semvar: SemanticVersion;
 }
 export type GBucketPrefixes = "tokens" | "crowdsales" | "guilds";
