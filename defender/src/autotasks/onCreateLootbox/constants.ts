93040c2c37dbde3d58b2ddb3696336d8c3bc9379
0xnewton <0xnewton@protonmail.com>
2022-03-26 21:01:28 -0400

    update manifest

--- defender/src/autotasks/onCreateLootbox/constants.ts
+++ defender/src/autotasks/onCreateLootbox/constants.ts
@@ -5,5 +5,6 @@ export const constants = {
     manifest.openZeppelin.autoTasks.onCreateLootbox.webhookEndpoint,
   AUTO_TASK_ID: manifest.openZeppelin.autoTasks.onCreateLootbox.id,
   FOLDER_NAME: manifest.openZeppelin.autoTasks.onCreateLootbox.slug,
+  // @todo remove this bish
   SECRET: "mysecret",
 };
