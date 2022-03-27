41428ee0196cad37fcb7e6952bf96bafbf885c1a
0xnewton <0xnewton@protonmail.com>
2022-03-27 02:27:13 -0400

    removed deprecated pd bullshit:

--- defender/deploy_deprecated/upload-sentinels.sh
+++
@@ -1,7 +0,0 @@
-echo "Deploying sentinels to OZ Defender..."
-yarn build
-
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-cd "$parent_path"
-
-NODE_ENV=production node ../lib/sentinels/onCreateLootbox/build.js
--- defender/deploy_deprecated/upload-tasks.sh
+++
@@ -1,7 +0,0 @@
-echo "Deploying tasks to OZ Defender..."
-yarn build
-
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-cd "$parent_path"
-
-NODE_ENV=production node ../lib/autotasks/onCreateLootbox/build.js 
