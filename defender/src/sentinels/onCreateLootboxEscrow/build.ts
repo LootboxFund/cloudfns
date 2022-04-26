2e985ad72f78ca855992c42c4c165606aba6ec67
0xnewton <0xnewton@protonmail.com>
2022-04-26 14:08:29 -0400

    update sentinels to deploy all tings accross chains

--- defender/src/sentinels/onCreateLootboxEscrow/build.ts
+++ defender/src/sentinels/onCreateLootboxEscrow/build.ts
@@ -8,7 +8,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import { SentinelClient } from "defender-sentinel-client";
-import { constants, sentinel } from "./constants";
+import { constants, sentinels } from "./constants";
 import { getDefenderApiCredentials } from "../../services/secrets"; // This file is not actually bundled into the autotask
 
 const main = async () => {
@@ -20,24 +20,28 @@ const main = async () => {
 
   const sentinelClient = new SentinelClient(credentials);
 
-  try {
-    console.log(`
-  
-  --- ⏳ Deploying the sentinel...
+  console.log(`
+    
+    --- ⏳ Deploying the sentinels...
   
   `);
-    console.log(`
 
---- watchOnCreateLootbox sentinel ---
-AutoTask ID: ${constants.AUTO_TASK_ID}
-Address: ${constants.SENTINAL_WATCH_ADDRESS}
-
-`);
-    const x = await sentinelClient.create(sentinel);
-    console.log(x);
-  } catch (e) {
-    console.log(e);
-  }
+  sentinels.map(async (sentinel) => {
+    try {
+      console.log(`
+  
+        --- watchOnCreateLootboxEscrow sentinel ---
+        ChainID: ${sentinel.network}
+        AutoTask ID: ${constants.AUTO_TASK_ID}
+        Address: ${sentinel.addresses?.join(", ")}
+        
+      `);
+      const x = await sentinelClient.create(sentinel);
+      console.log(x);
+    } catch (e) {
+      console.log(e);
+    }
+  });
 };
 
 main();
