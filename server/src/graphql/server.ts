ba8758dfa9f126b514f9a1f09615c0c087e50a6c
0xnewton <0xnewton@protonmail.com>
2022-05-29 14:04:17 -0400

    add context in gql server

--- server/src/graphql/server.ts
+++ server/src/graphql/server.ts
@@ -1,5 +1,18 @@
 import { ApolloServer } from "apollo-server";
+import identityProvider from "../api/identityProvider";
+import { IIdpUser } from "../api/identityProvider/interface";
 import { schema } from "./schema";
+
+const decodeToken = (token: string) => {
+  try {
+    const jsonToken = JSON.parse(Buffer.from(token, "base64").toString("utf8"));
+
+    return JSON.parse(jsonToken);
+  } catch (error) {
+    // TODO replace later with logger
+    return null;
+  }
+};
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
@@ -7,7 +20,39 @@ const server = new ApolloServer({
   schema,
   csrfPrevention: true,
   introspection: true,
-  //   context: async ({req}) => {}
+  context: async ({ req }): Promise<{ userId: string | null }> => {
+    try {
+      const { authorization } = req.headers;
+
+      if (!authorization) {
+        return { userId: null };
+      }
+
+      const token = authorization.split(" ")[1];
+      const decodedToken = decodeToken(token);
+
+      if (
+        !decodedToken ||
+        !decodedToken.idToken ||
+        !decodedToken.refreshToken
+      ) {
+        return { userId: null };
+      }
+
+      const idpId = await identityProvider.verifyIDToken(
+        decodedToken.idToken,
+        decodedToken.refreshToken
+      );
+
+      if (!idpId) {
+        return { userId: null };
+      } else {
+        return { userId: idpId };
+      }
+    } catch (_err) {
+      return { userId: null };
+    }
+  },
 });
 
 export default server;
