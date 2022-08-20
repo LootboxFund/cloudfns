230bd69f8e2d3def0c047e5f8326cb779bfb2a61
0xnewton <0xnewton@protonmail.com>
2022-08-20 06:22:14 -0400

    fix bug x2

--- lootbox-server/src/graphql/schema/referral/resolvers.ts
+++ lootbox-server/src/graphql/schema/referral/resolvers.ts
@@ -111,12 +111,12 @@ const ReferralResolvers: Resolvers = {
   Claim: {
     userLink: async (claim: Claim): Promise<PublicUser | null> => {
       if (claim.type === ClaimType.Referral) {
-        if (!claim.claimerUserId) {
+        if (!claim.referrerId) {
           return null;
         }
 
         try {
-          const user = await getUser(claim.claimerUserId);
+          const user = await getUser(claim.referrerId);
           const publicUser = user ? convertUserToPublicUser(user) : null;
 
           return publicUser;
@@ -125,12 +125,12 @@ const ReferralResolvers: Resolvers = {
           return null;
         }
       } else if (claim.type === ClaimType.Reward) {
-        if (!claim.claimerUserId) {
+        if (!claim.rewardFromFriendReferred) {
           return null;
         }
 
         try {
-          const user = await getUser(claim.claimerUserId);
+          const user = await getUser(claim.rewardFromFriendReferred);
           const publicUser = user ? convertUserToPublicUser(user) : null;
 
           return publicUser;
