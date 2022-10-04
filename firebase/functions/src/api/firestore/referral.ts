4864d9f5e1c8a8654bc1d802428762cc53ec2df7
0xnewton <0xnewton@protonmail.com>
2022-10-03 21:12:45 -0400

    remove unused code

--- firebase/functions/src/api/firestore/referral.ts
+++ firebase/functions/src/api/firestore/referral.ts
@@ -101,27 +101,6 @@ export const createRewardClaim = async (req: CreateRewardClaimReq): Promise<Clai
     return newClaim;
 };
 
-export const associateWhitelistToClaim = async (
-    referralID: ReferralID,
-    claimID: ClaimID,
-    whitelistID: LootboxMintWhitelistID
-): Promise<void> => {
-    const ref = db
-        .collection(Collection.Referral)
-        .doc(referralID)
-        .collection(Collection.Claim)
-        .doc(claimID) as DocumentReference<Claim_Firestore>;
-
-    const updateRequest: Partial<Claim_Firestore> = {
-        whitelistId: whitelistID,
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        "timestamps.updatedAt": Timestamp.now().toMillis(),
-    };
-
-    await ref.update(updateRequest);
-};
-
 export const getUnassignedClaimsForUser = async (
     claimerUserID: UserID
     // lootboxID: LootboxID
