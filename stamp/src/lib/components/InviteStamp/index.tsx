4649e70576dd275c69f9316d9337c4ef1f4d8495
0xterran <0xterran@protonmail.com>
2022-10-15 20:44:54 +0700

    alpha stamp

---
+++ stamp/src/lib/components/InviteStamp/index.tsx
@@ -0,0 +1,211 @@
+import { FunctionComponent } from "react";
+import "./index.css";
+
+const BCSRRGMSRaysterG: FunctionComponent = () => {
+  return (
+    <article className="bcs-rrgms-rayster-g" id="invite-graphic">
+      <div className="group-div">
+        <img
+          className="rectangle-icon"
+          alt=""
+          src="https://firebasestorage.googleapis.com/v0/b/lootbox-fund-staging.appspot.com/o/shared-company-assets%2FStampLootbox_Classic%2Frectangle-97%402x.png?alt=media&token=84b8959e-86d1-46d8-aa43-67ccc20089cf"
+        />
+        <div className="rectangle-div" />
+        <img
+          className="sponsor-icon"
+          alt=""
+          src="https://firebasestorage.googleapis.com/v0/b/lootbox-fund-staging.appspot.com/o/shared-company-assets%2FStampLootbox_Classic%2Fsponsor%402x.png?alt=media&token=53578be4-4031-413f-b17a-ef353986478e"
+        />
+        <div className="mask-group-div">
+          <div className="group-div1">
+            <div className="group-div2">
+              <img
+                className="rectangle-icon1"
+                alt=""
+                src="https://firebasestorage.googleapis.com/v0/b/lootbox-fund-staging.appspot.com/o/shared-company-assets%2FStampLootbox_Classic%2Frectangle-1%402x.png?alt=media&token=4ddb59ae-220d-4eed-922e-0526b4b806bf"
+              />
+              <img
+                className="group-286-2-1"
+                alt=""
+                src="https://firebasestorage.googleapis.com/v0/b/lootbox-fund-staging.appspot.com/o/shared-company-assets%2FStampLootbox_Classic%2Fgroup-286-2-1%402x.png?alt=media&token=87e99d27-80a1-4e0c-8e92-4716f4f9219f"
+              />
+              <div className="rectangle-div1" />
+              <div className="lootbox-div">LOOTBOX</div>
+              <div className="cabanatuan-tournament-div">
+                CABANATUAN TOURNAMENT
+              </div>
+              <div className="fan-prize-500-php">
+                <span className="fan-prize-500-container">
+                  <p className="fan-prize">
+                    <span className="php-span">
+                      <span>{`FAN PRIZE `}</span>
+                    </span>
+                  </p>
+                  <p className="php">
+                    <span className="php-span">
+                      <span>{`500 PHP `}</span>
+                    </span>
+                  </p>
+                </span>
+              </div>
+              <div className="fan-lootbox-ticket">Fan LOOTBOX Ticket</div>
+              <div className="game-axie-infinity-date-octo">
+                <span className="fan-prize-500-container">
+                  <p className="fan-prize">Game: Axie Infinity</p>
+                  <p className="fan-prize">{`Date: October 22, 2022 `}</p>
+                  <p className="at-12-nn">at 12 NN - 6PM</p>
+                </span>
+              </div>
+              <div className="audience-members-can-win-a-por">
+                <b>Audience</b>
+                <span className="members-can-win">{` members can win a portion of the `}</span>
+                <b>prize money</b>
+                <span className="members-can-win">
+                  {" "}
+                  if this team wins the tournament.
+                </span>
+              </div>
+            </div>
+            <div className="event-3pg-mlbb-tournament-dat">
+              <span className="fan-prize-500-container">
+                <p className="fan-prize">
+                  <b className="event-b">Event:</b>
+                  <span> 3PG MLBB Tournament</span>
+                </p>
+                <p className="at-12-nn">
+                  <b className="event-b">{`Date: `}</b>
+                  <span>7:30pm Wednesday August 3rd 2022</span>
+                </p>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="mask-group-div1">
+          <div className="group-div1">
+            <div className="group-div2">
+              <img
+                className="rectangle-icon1"
+                alt=""
+                src="https://firebasestorage.googleapis.com/v0/b/lootbox-fund-staging.appspot.com/o/shared-company-assets%2FStampLootbox_Classic%2Frectangle-1%402x.png?alt=media&token=4ddb59ae-220d-4eed-922e-0526b4b806bf"
+              />
+              <img
+                className="group-286-2-1"
+                alt=""
+                src="https://firebasestorage.googleapis.com/v0/b/lootbox-fund-staging.appspot.com/o/shared-company-assets%2FStampLootbox_Classic%2Fgroup-286-2-1%402x.png?alt=media&token=87e99d27-80a1-4e0c-8e92-4716f4f9219f"
+              />
+              <div className="rectangle-div1" />
+              <span className="game-date-span" id="game-date">
+                <span className="fan-prize-500-container">
+                  <p className="fan-prize">&nbsp;</p>
+                  <p className="fan-prize">{`Date: October 22, 2022 `}</p>
+                  <p className="at-12-nn">at 12 NN - 6PM</p>
+                </span>
+              </span>
+              <div className="lootbox-div1">LOOTBOX</div>
+              <span className="subtitle-span" id="sub-title">
+                CABANATUAN TOURNAMENT
+              </span>
+              <div className="fan-prize-div">
+                <span className="php-span">
+                  <span>{`FAN PRIZE `}</span>
+                  <span className="span">{` `}</span>
+                </span>
+              </div>
+              <div className="fan-lootbox-ticket1">Fan LOOTBOX Ticket</div>
+              <span className="game-name-span" id="game-name">
+                Game: Axie Infinity
+              </span>
+              <div className="audience-members-can-win-a-por">
+                <b>Audience</b>
+                <span className="members-can-win">{` members can win a portion of the `}</span>
+                <b>prize money</b>
+                <span className="members-can-win">
+                  {" "}
+                  if this team wins the tournament.
+                </span>
+              </div>
+            </div>
+            <div className="event-3pg-mlbb-tournament-dat1">
+              <span className="fan-prize-500-container">
+                <p className="fan-prize">
+                  <b className="event-b">Event:</b>
+                  <span> 3PG MLBB Tournament</span>
+                </p>
+                <p className="at-12-nn">
+                  <b className="event-b">{`Date: `}</b>
+                  <span>7:30pm Wednesday August 3rd 2022</span>
+                </p>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="mask-group-div2">
+          <img
+            className="axie-logo-1"
+            alt=""
+            src="https://firebasestorage.googleapis.com/v0/b/lootbox-fund-staging.appspot.com/o/shared-company-assets%2FStampLootbox_Classic%2Fgame-logo%402x.png?alt=media&token=d7f91904-7401-422c-b405-2f48ad7d072b"
+          />
+          <img
+            className="be9b80-4957-4612-bda8-7d391c40-icon"
+            alt=""
+            src="https://firebasestorage.googleapis.com/v0/b/lootbox-fund-staging.appspot.com/o/shared-company-assets%2FStampLootbox_Classic%2Fgame-asset%402x.png?alt=media&token=367def89-ff4b-4e35-a7b3-511c70d08347"
+          />
+          <div className="group-div5">
+            <div className="group-div6">
+              <div className="rectangle-div3" />
+              <img
+                className="qr-code-icon"
+                alt=""
+                src="https://firebasestorage.googleapis.com/v0/b/lootbox-fund-staging.appspot.com/o/shared-company-assets%2FStampLootbox_Classic%2Fqr-code%402x.png?alt=media&token=2938548b-8aee-4241-aa40-cdc1467d3481"
+              />
+            </div>
+            <a className="group-a" href="invite-link">
+              <div className="link-div">
+                <div className="rectangle-div4" />
+                <b className="golootboxfundrrmm23qp4lyo">
+                  🔒 go.lootbox.fund/r?r=mm23QP4lYO
+                </b>
+              </div>
+            </a>
+          </div>
+          <div className="group-div7">
+            <div className="rectangle-div5" />
+            <b className="scan-or-screenshot-to-open-qr">
+              Scan or Screenshot to Open QR Code
+            </b>
+          </div>
+        </div>
+        <div className="group-div8">
+          <div className="shares-the-tournament-cash-pri">
+            Shares the tournament cash prize with the holder of this fan LOOTBOX
+            ticket
+          </div>
+          <h2 className="team-nam-e" id="team-name">
+            RRGMS | Rayster G.
+          </h2>
+        </div>
+        <img
+          className="vector-icon"
+          alt=""
+          src="https://firebasestorage.googleapis.com/v0/b/lootbox-fund-staging.appspot.com/o/shared-company-assets%2FStampLootbox_Classic%2Fvector-1.svg?alt=media&token=8b28a1f3-8499-4294-b2e4-d456ee9b50d4"
+        />
+      </div>
+      <img
+        className="lootbox-image-icon"
+        alt=""
+        src="https://firebasestorage.googleapis.com/v0/b/lootbox-fund-staging.appspot.com/o/shared-company-assets%2FStampLootbox_Classic%2Flootbox-image%402x.png?alt=media&token=02af931c-039b-4b7b-a215-0ece3a4e936d"
+      />
+      <div className="the-logo-solo-perfected-1" />
+      <img
+        className="headshot-icon"
+        alt=""
+        src="https://firebasestorage.googleapis.com/v0/b/lootbox-fund-staging.appspot.com/o/shared-company-assets%2FStampLootbox_Classic%2Fheadshot-photo%402x.png?alt=media&token=7ee12bf7-0efe-47cc-b2da-87e915249dee"
+      />
+      <h1 className="prize-amount-h1" id="prize-amount">
+        <span className="php-span">500 PHP</span>
+      </h1>
+    </article>
+  );
+};
+
+export default BCSRRGMSRaysterG;
