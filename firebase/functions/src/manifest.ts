7a41fdb959c19b3d5dfdcb2a14331badc12462eb
0xnewton <0xnewton@protonmail.com>
2022-11-28 20:09:39 -0500

    pin manifest

--- firebase/functions/src/manifest.ts
+++ firebase/functions/src/manifest.ts
@@ -1,3 +1,4 @@
-import { latest as Manifest, SecretName } from "@wormgraph/manifest";
+import { Manifest_v0_7_3_prod as Manifest, SecretName } from "@wormgraph/manifest";
+// import { latest as Manifest, SecretName } from "@wormgraph/manifest";
 export const manifest = Manifest.default;
 export { SecretName };
